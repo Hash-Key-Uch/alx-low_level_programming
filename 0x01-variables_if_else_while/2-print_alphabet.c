c5672b26782d9602adb6d30d2b3f006841e55150
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-09-09 14:47:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Prints alphabets in lowercase
+ *
+ * Returns: Always (0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+
+		putchar('\n');
+
+	return (0);
+
+}
