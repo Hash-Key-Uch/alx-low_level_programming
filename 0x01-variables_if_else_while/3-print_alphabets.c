6ea7c744d32b45fa7002b2d687088da0ecbc7dc7
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-09-09 15:43:12 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * followed by a new line
+ * Returns: Always (0)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int x;
+
+for (x = 0; x < 52; x++);
+
+	{
+
+		putchar(alp[x]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
