c4dd83e2d6729ce268900798f5f0d3b9db1352cc
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-09-13 10:46:10 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,11 +6,12 @@
  * 0 if otherwise
  * 97 to 122 = 'a' - 'z'
  * hint: The ASCII code was used.
- *
+ * int _islower (int c) is a prototype
+ * @c : uses int _islower(int c)
  * Return: 1 for lowercase character. 0 for the rest.
  */
 
-int _islower(int c)
+int _islower(int c) /*This is the prototype for this question*/
 {
 	if (c >= 97 && c <= 122)
 
