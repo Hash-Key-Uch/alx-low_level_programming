5feea3efbcc752b4e64fbc779d30f1eb9a14e176
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-09-22 16:05:39 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 
-
+/**
+* Main - Entry point
+* Return: Always 0 (success)
+*
+*/
 
 int main(void)
 
 {
 
-  int n;
+int n;
 
-  int a[5];
+int a[5];
 
-  int *p;
+int *p;
 
 
 
-  a[2] = 1024;
+a[2] = 1024;
 
-  p = &n;
+p = &n;
 
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+*   main: entry point
+* - you are not allowed to code anything else than this line of code
+*/
 
-  ;
 
-  p[5] = 98;
-  /* ...so that this prints 98\n */
+*(p + 5) = 98;
 
-  printf("a[2] = %d\n", a[2]);
+/* ...so that this prints 98\n */
 
-  return (0);
+printf("a[2] = %d\n", a[2]);
+
+return (0);
 
 }
