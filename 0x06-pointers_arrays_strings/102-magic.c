490b88a7f91cec1c54d1bfc48d60c6bd6b93016c
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-09-22 17:03:14 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -20,7 +20,7 @@ int *p;
 a[2] = 1024;
 
 p = &n;
-  
+
 /*
 * write your line of code here...
 * Remember:
